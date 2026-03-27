eec981ee7980697004e4b6b652ce856b86999092
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-03-26 20:15:43 -0400

    Updates

--- assets/BusinessCard3D-BS9wsGAp.js
+++ assets/BusinessCard3D-BS9wsGAp.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-e9BYNFvQ.js";/**
+import{r as Pi,j as mr}from"./index-CB9uCBPT.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
