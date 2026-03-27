ff1e4a842f81668d24366b57c75553d4640a6a83
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-03-26 23:48:54 -0400

    Updates

--- assets/BusinessCard3D-CrzakYPt.js
+++ assets/BusinessCard3D-CrzakYPt.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-Cg9XI16K.js";/**
+import{r as Pi,j as mr}from"./index-8SWwSmrX.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
