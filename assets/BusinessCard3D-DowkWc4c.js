5e41179f96db12a74a172c1def2bc835889029b6
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-03-30 14:20:26 -0400

    Updates

--- assets/BusinessCard3D-DowkWc4c.js
+++ assets/BusinessCard3D-DowkWc4c.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-DB8yccrk.js";/**
+import{r as Pi,j as mr}from"./index-6hRm4eD2.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/peakaquatics1/sw.js', { scope: '/peakaquatics1/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
