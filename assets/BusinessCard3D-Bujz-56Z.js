f5ba917b45d75ba0e45a4beaadfdd3d5e72b2c69
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-03-31 17:34:03 -0400

    Updates

--- assets/BusinessCard3D-Bujz-56Z.js
+++ assets/BusinessCard3D-Bujz-56Z.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-CnXnpKih.js";/**
+import{r as Pi,j as mr}from"./index-D9Cil4li.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
