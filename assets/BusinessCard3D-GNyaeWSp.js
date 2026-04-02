454d732bb05039049882775a8dbba37b0461ebd2
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-04-02 16:51:08 -0400

    Updates

--- assets/BusinessCard3D-GNyaeWSp.js
+++ assets/BusinessCard3D-GNyaeWSp.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-a9-TiGK9.js";/**
+import{r as Pi,j as mr}from"./index-C3PtkM44.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
