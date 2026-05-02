eb84fb90bdb45de71edbe8730169e61cc0f98576
brianjdope <145787578+brianjdope@users.noreply.github.com>
2026-05-02 10:59:12 -0400

    Updates

--- assets/BusinessCard3D-CamTph6n.js
+++ assets/BusinessCard3D-CamTph6n.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-DeH9Kh1H.js";/**
+import{r as Pi,j as mr}from"./index-cdIwE6R9.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
