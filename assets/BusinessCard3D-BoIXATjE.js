6bca0c00eb433308c92f530d551ed242f9a3c388
Philip Kang <philipkang@Philips-MacBook-Pro.local>
2026-06-25 11:12:01 -0400

    Updates

--- assets/BusinessCard3D-BoIXATjE.js
+++ assets/BusinessCard3D-BoIXATjE.js
@@ -1,4 +1,4 @@
-import{r as Pi,j as mr}from"./index-CvK3DLJb.js";/**
+import{r as Pi,j as mr}from"./index-7tjaw-TN.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
